5cd2377d045728df6de4cb924578e2c52fd2d426
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-27 16:57:04 +0900

    체크 박스 - 단일1

--- src/main/java/spring/itemservice/web/form/FormItemController.java
+++ src/main/java/spring/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package spring.itemservice.web.form;
 
+import lombok.extern.slf4j.Slf4j;
 import spring.itemservice.domain.item.Item;
 import spring.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,7 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
